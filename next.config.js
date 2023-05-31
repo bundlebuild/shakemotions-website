145c4a54b0891bb611bfed6ac483bbd6c7277613
bundlebuild <arnavnath550@gmail.com>
2023-05-31 13:21:54 +1200

    netfliy support added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Add the following lines to your existing config or create a new config object
-  output: {
-    // Set the output directory to 'out' for static HTML export
-    dir: 'out',
-  },
+  target: "serverless",
 };
