dab0cb6cf567418aefc7029610ca395f9367acb6
bundlebuild <arnavnath550@gmail.com>
2023-05-31 13:14:45 +1200

    next.config.js file updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-}
-
-module.exports = nextConfig
+module.exports = {
+  // Add the following lines to your existing config or create a new config object
+  output: {
+    // Set the output directory to 'out' for static HTML export
+    dir: 'out',
+  },
+};
